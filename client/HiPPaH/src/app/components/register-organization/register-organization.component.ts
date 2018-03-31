840665e1b01f0e8bf55d11dfa9a3a47c09c08343
Petar Metodiev <petar.metodiev@gmail.com>
2018-03-31 18:39:44 +0300

    Wire up all forms

--- client/HiPPaH/src/app/components/register-organization/register-organization.component.ts
+++ client/HiPPaH/src/app/components/register-organization/register-organization.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit
+} from '@angular/core';
+import {
+	FormControl,
+	FormGroup,
+	Validators
+} from '@angular/forms';
 
 @Component({
 	selector: 'hip-register-organization',
@@ -7,12 +15,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterOrganizationComponent implements OnInit {
 
+	form: FormGroup;
+	name: FormControl;
+
 	constructor() { }
 
 	ngOnInit() {
+		this.name = new FormControl(
+			'',
+			[Validators.required, Validators.minLength(3)]
+		);
+
+		this.form = new FormGroup({
+			name: this.name
+		});
 	}
 
 	register() {
-		console.log('registering organization...');
+		console.log(this.form.value);
+		console.log('Is name valid? ', this.name.valid);
 	}
 }
--- client/HiPPaH/src/app/components/register-user/register-user.component.ts
+++ client/HiPPaH/src/app/components/register-user/register-user.component.ts
@@ -3,6 +3,12 @@ import {
 	OnInit
 } from '@angular/core';
 
+import {
+	FormControl,
+	FormGroup,
+	Validators
+} from '@angular/forms';
+
 @Component({
 	selector: 'hip-register',
 	templateUrl: './register-user.component.html',
@@ -10,9 +16,37 @@ import {
 })
 export class RegisterUserComponent implements OnInit {
 
+	form: FormGroup;
+	email: FormControl;
+	password: FormControl;
+	verifyPassword: FormControl;
+
 	constructor() { }
 
 	ngOnInit() {
+		this.email = new FormControl(
+			'',
+			[Validators.required, Validators.email]
+		);
+
+		this.password = new FormControl(
+			'',
+			[Validators.required, Validators.minLength(8)]
+		);
+
+		this.verifyPassword = new FormControl(
+			'',
+			[Validators.required]
+		);
+
+		this.form = new FormGroup({
+			email: this.email,
+			password: this.password,
+			verifyPassword: this.verifyPassword
+		});
 	}
 
+	register() {
+		console.log(this.form.value);
+	}
 }
