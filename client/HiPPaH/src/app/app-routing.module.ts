b3e6998dfafaf8878c0d6e316d1383e623081250
Petar Metodiev <petar.metodiev@gmail.com>
2018-03-31 14:38:52 +0300

    Add scanner component

--- client/HiPPaH/src/app/app-routing.module.ts
+++ client/HiPPaH/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { RegisterOrganizationComponent } from './components/register-organization/register-organization.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ScannerComponent } from './components/scanner/scanner.component';
 
 const routes: Routes = [
 	{
@@ -21,6 +22,10 @@ const routes: Routes = [
 	{
 		path: 'profile',
 		component: ProfileComponent
+	},
+	{
+		path: 'scan',
+		component: ScannerComponent
 	}
 ];
 
--- client/HiPPaH/src/app/app.module.ts
+++ client/HiPPaH/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Scanner lib for qr codes.
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -8,6 +11,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { RegisterOrganizationComponent } from './components/register-organization/register-organization.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ScannerComponent } from './components/scanner/scanner.component';
 
 
 @NgModule({
@@ -17,10 +21,12 @@ import { ProfileComponent } from './components/profile/profile.component';
 		RegisterUserComponent,
 		RegisterOrganizationComponent,
 		ProfileComponent,
+		ScannerComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		ZXingScannerModule.forRoot(),
 	],
 	providers: [],
 	bootstrap: [AppComponent]
