cdd0fa48f77e8ad3ca2f7d16eb1b3f66da6520fe
Petar Metodiev <petar.metodiev@gmail.com>
2018-03-31 10:23:35 +0300

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const User = require('./../models/User')
+
+module.exports = () => {
+  // mongoose.connect('mongodb://localhost:27017/demo-server-db')
+  mongoose.connect(
+    'mongodb://admin:admin@ds229909.mlab.com:29909/pay-safe-hack'
+  )
+
+  const db = mongoose.connection
+
+  db.once('open', err => {
+    if (err) {
+      throw err
+    }
+
+    console.log('Db connected')
+  })
+
+  db.on('error', reason => {
+    console.log(reason)
+  })
+}
